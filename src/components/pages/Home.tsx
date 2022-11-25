fe6e229cb1af05817010e70e68f8988b90fea787
amitsafi45 <safiamit45@gmail.com>
2022-11-26 01:16:17 +0545

    feat: design UI for add student detail

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Typography,Box,Grid,TableContainer,Table,TableBody,TableCell,TableHead,TableRow,Paper
+,Tooltip,TextField,Button,IconButton} from '@mui/material/'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import {deepPurple,green,orange} from '@mui/material/colors'
+import { makeStyles, Theme, createStyles } from '@mui/material/styles'; 
+
+// const useStyles:any=makeStyles({
+//   headingColor:{
+//     backgroundColor: deepPurple[400],
+//     color:"white"
+//   }
+// })
+export const  Home = () => {
+  return (
+    <>
+      <Box textAlign='center' sx={{
+        backgroundColor:deepPurple[400],
+        color:"white"
+      }} p={2} mb={2}>
+        <Typography  variant='h1'>React CRUD with API Call</Typography>
+      </Box>
+      <Grid container>
+          <Grid item md={6} xs={12} >
+          <Box textAlign="center" p={2} sx={{backgroundColor:green[400],colo:"white"}} mb={2}>
+            <Typography variant="h4">Add student Detail</Typography>
+
+          </Box>
+          <form noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} >
+                <TextField autoComplete='stuName' name='stuName' variant='outlined'required fullWidth id="stuName" label="Name"/>
+              </Grid>
+              <Grid item xs={12} >
+                <TextField autoComplete='email' name='email' variant='outlined'required fullWidth id="email" label="email"/>
+              </Grid>
+              
+            </Grid>
+            <Box m={1}>
+              <Button type="submit" variant='contained' color="primary" fullWidth>Add</Button>
+            </Box>
+          </form>
+          </Grid>
+              <h1>studentList</h1>
+          <Grid item md={6} xs={12} >
+
+          </Grid>
+        </Grid>
+    </>
+    )
+}
