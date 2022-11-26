2b2d97b426582c1865ff0c2c1e6c9399273c709a
amitsafi45 <safiamit45@gmail.com>
2022-11-26 21:57:35 +0545

    pref: write separately view feature from home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -9,6 +9,7 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import { deepPurple, green, orange } from '@mui/material/colors'
 import { makeStyles, Theme, createStyles } from '@mui/material/styles';
 import {Link} from 'react-router-dom'
+import { List } from '../student/List'
 // const useStyles:any=makeStyles({
 //   headingColor:{
 //     backgroundColor: deepPurple[400],
@@ -47,59 +48,8 @@ export const Home = () => {
         </Grid>
         
         <Grid item md={6} xs={12} >
-
-          <Box textAlign="center" p={2} sx={{
-            backgroundColor: orange[400],
-            color: "white"
-          }}>
-            <Typography variant="h4">Student List</Typography>
-          </Box>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow style={{ backgroundColor: "#616161" }}>
-                  <TableCell align="center" sx={{
-                    color: "white",
-                    fontWeight: "bold",
-                    fontSize: 16
-                  }}>No</TableCell>
-                  <TableCell align="center" sx={{
-                    color: "white",
-                    fontWeight: "bold",
-                    fontSize: 16
-                  }}>Name</TableCell>
-                  <TableCell align="center" sx={{
-                    color: "white",
-                    fontWeight: "bold",
-                    fontSize: 16
-                  }}>Email</TableCell>
-                  <TableCell align="center" sx={{
-                    color: "white",
-                    fontWeight: "bold",
-                    fontSize: 16
-                  }}>Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-              <TableRow>
-          <TableCell align="center">1</TableCell>
-          <TableCell align="center">Amit Safi</TableCell>
-          <TableCell align="center">amitsafiData@gmail.com</TableCell>
-          <TableCell align="center">
-           <Tooltip title="View">
-            <IconButton><Link to="/view/1"><VisibilityIcon color="primary" /></Link></IconButton>
-           </Tooltip>
-           <Tooltip title="Edit">
-            <IconButton><Link to="/edit/1"><EditIcon /></Link></IconButton>
-           </Tooltip>
-           <Tooltip title="Delete">
-            <IconButton ><DeleteIcon color="secondary" /></IconButton>
-           </Tooltip>
-          </TableCell>
-         </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
+           <List/>
+          
         </Grid>
         </Grid>    
     </>
